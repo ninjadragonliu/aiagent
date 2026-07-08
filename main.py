f07b94edb69cc3213bab307c98614e55ccb34154
ninjadragonliu <dragoncool.liu@gmail.com>
2026-07-08 11:51:19 -0400

    changed from gemini to openAi

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
-from google import genai
+from openai import OpenAI
 
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key = os.environ.get("OPENROUTER_API_KEY")
     if not api_key:
-        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+        raise RuntimeError("OPENROUTER_API_KEY environment variable not set")
 
-    client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
     )
-    if response.usage_metadata is None:
-        raise RuntimeError("Gemini API response appears to be malformed")
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+    if response.usage is None:
+        raise RuntimeError("OpenAi SDK response appears to be malformed")
 
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    if args.verbose is True:
+        print("User prompt: " + args.user_prompt)
+        print("Prompt tokens: " + str(response.usage.prompt_tokens))
+        print("Response tokens: " + str(response.usage.completion_tokens))
 
-    print("Response:")
-    print(response.text)
 
 
 if __name__ == "__main__":
